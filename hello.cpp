66bd750c6883f4670748f68da656d7dc58d1c2ef
jwBaek <gross1234@hanyang.ac.kr>
2023-12-08 16:14:12 +0900

    test1

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main()
 {
+<<<<<<< HEAD
 	cout << "this is best solution" << endl;
+=======
+	printf("hello.c\n");
+	cout << "hello kcci" << endl;
+>>>>>>> parent of 6b6672b (change message in hello.cpp)
 	return 0;
 }
