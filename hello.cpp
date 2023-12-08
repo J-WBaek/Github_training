67a36e115405486cf439ec26016b9a7437cd7ca5
jwBaek <gross1234@hanyang.ac.kr>
2023-12-08 16:05:20 +0900

    revert again

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main()
 {
+<<<<<<< HEAD
 	cout << "this is best solution" << endl;
 	cout << "this is not a solution" << endl;
+=======
+	printf("hello.c\n");
+	cout << "hello kcci" << endl;
+>>>>>>> parent of 6b6672b (change message in hello.cpp)
 	return 0;
 }
